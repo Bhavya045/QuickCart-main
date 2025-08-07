10acac8a251e4671fea1cb97c88e621475661ea2
Bhavya045 <backupstorage114@gmail.com>
2025-08-07 22:19:53 +0530

    Update route.js 1

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,7 +1,7 @@
 import { inngest } from "@/config/inngest";
 import Product from "@/model/Product";
 import User from "@/model/User";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
  
